8413ea841357b8046570f814af89c3738de3e565
p17166682 <p17166682@GWHXX679WIN012W>
2020-03-12 17:56:38 +0000

    have made a big change to the system,delete the config and add new item using the toolbox

--- MyMirror-frontoffice/AStock.aspx.cs
+++ MyMirror-frontoffice/AStock.aspx.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MyMirror_classes;
 
 public partial class AStock : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+    }
+
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        clsStock Astock = new clsStock();
+
+        Astock.StockDes = txtDes.Text;
+        Astock.Price = txtPrice.Text;
+        Astock.Quantity = txtQuantity.Text;
+
 
     }
 }
